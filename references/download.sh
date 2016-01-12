be645494a5b9f44064dd8491f65413cc0d415319
Thomas Levine <_@thomaslevine.com>
2016-01-12 18:23:52 +0000

    download.sh

---
+++ references/download.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+wget http://www.cymaticaudio.com/images/downloads/manuals_and_brochures/cymatic%20LR%2016%20brochure%202.pdf
+wget http://www.cymaticaudio.com/images/downloads/manuals_and_brochures/LR16_ProductManual_EN.pdf
+wget http://www.cymaticaudio.com/images/downloads/manuals_and_brochures/LR%2016%20Quick_start_guide_20130109.pdf
+wget http://www.cymaticaudio.com/images/downloads/SC08GLColor.pdf
+wget http://cymaticaudio.com/forum/liverecorderlr-16/17-help
